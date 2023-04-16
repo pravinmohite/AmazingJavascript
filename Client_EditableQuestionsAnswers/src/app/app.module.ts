c2e19cfc54fca35ca5a8b07d838d6bd01e3b34ce
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2023-04-16 17:46:23 +0530

    added signup form controls

--- Client_EditableQuestionsAnswers/src/app/app.module.ts
+++ Client_EditableQuestionsAnswers/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './partial_views/header/header.component';
@@ -27,6 +27,7 @@ import { SignupComponent } from './partial_views/modals/signup/signup.component'
     HttpClientModule,
     FormsModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
     ModalModule.forRoot()
   ],
   providers: [],
--- Client_EditableQuestionsAnswers/src/app/partial_views/modals/signup/signup.component.ts
+++ Client_EditableQuestionsAnswers/src/app/partial_views/modals/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,9 +11,22 @@ export class SignupComponent implements OnInit {
   title?: string;
   closeBtnName?: string;
   list: any[] = [];
-  constructor(public bsModalRef: BsModalRef) {}
+  signupForm: FormGroup;
+  constructor(
+    public bsModalRef: BsModalRef,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    });
+  }
+
+  handleFormSubmit(formValue:any): void{
+    console.log(formValue);
   }
 
 }
