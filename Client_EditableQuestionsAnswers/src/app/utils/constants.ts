18b974541fd4b8f5127e279a3a23fdc894c3c9a0
pravinmohite <pravinmohiteait@gmail.com>
2023-08-04 23:49:49 +0530

    new changes

--- Client_EditableQuestionsAnswers/src/app/utils/constants.ts
+++ Client_EditableQuestionsAnswers/src/app/utils/constants.ts
@@ -22,7 +22,7 @@ export const topCSS3InterviewQuestions = {
 
 export const topJavascriptInterviewQuestions = {
     path: 'top-javascript-interview-questions-and-answers',
-    pathOld: 'top-100%2B-javascript-interview-questions-and-answers',
+    pathOld: 'top-100+-javascript-interview-questions-and-answers',
     title: 'Top 100+ Javascript Interview Questions And Answers',
     headingTitle: 'Top 100+ Javascript Interview Questions And Answers',
     type: 'Javascript'
