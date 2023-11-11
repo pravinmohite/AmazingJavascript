c7671139c6b051093a01cc625e032de92f99375c
pravinmohite <pravinmohiteait@gmail.com>
2023-11-11 20:02:25 +0530

    my post changes

---
+++ Client_EditableQuestionsAnswers/src/app/user-post/my-post/my-post.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-post',
+  templateUrl: './my-post.component.html',
+  styleUrls: ['./my-post.component.scss']
+})
+export class MyPostComponent implements OnInit {
+
+  isMyPost = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
